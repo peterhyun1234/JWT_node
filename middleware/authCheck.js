60320442141c542b024d4b1fd57b736d9a3e95f2
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-01-30 22:31:00 +0900

    after applying token system

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -1,17 +1,18 @@
 var jwt = require('jsonwebtoken');
-var secretObj = require('./config/jwt');
+var secretObj = require('../config/jwt');
 
 exports.verifyToken = (req, res, next) => {
-
-    try {
-        var decoded = jwt.verify(req.cookies.user, secretObj.secret);
-        res.loginFlag1 = 1;
-        next();
-    } catch (err) {
-        if (err.name === 'TokenExpiredError') {
-            return res.redirect('/login?allowed=jwt_expired');
-        }
-        res.loginFlag1 = 0;
-        next();
-    }
+    res.tokenStatus = 1;
+    next();
+    // try {
+    //     // /var decoded = jwt.verify(req.cookies.user, secretObj.secret);
+    //     res.tokenStatus = 1;
+    //     next();
+    // } catch (err) {
+    //     if (err.name === 'TokenExpiredError') {
+    //         console.log("TokenExpiredError");
+    //     }
+    //     res.tokenStatus = 0;
+    //     next();
+    // }
 };
--- router/apis.js
+++ router/apis.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var { verifyToken } = require('../middleware/authCheck');
 
 
-router.get('/get_test', function(req, res) {
+router.get('/get_test', verifyToken, function(req, res) {
 
+    var tokenStatus = req.tokenStatus; // verifyToken을 거친 토큰 상태
+    console("tokenStatus: " + tokenStatus);
     var data = "get test";
     res.send(data);
 
 });
 
 
-router.post("/post_test", function(req, res, next) {
+router.post("/post_test", verifyToken, function(req, res, next) {
 
+    var tokenStatus = res.tokenStatus;
+    console("tokenStatus: " + tokenStatus);
     //var recv_code = req.body.p_code;
     var data = "post test";
     res.send(data);
